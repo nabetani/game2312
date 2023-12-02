0a4f12ffaf4ff0eacea8ffc49b317e172614d470
Nabetani <takenori@nabetani.sakura.ne.jp>
2023-12-02 19:15:14 +0900

    waiting に背景入れる

--- src/waiting.ts
+++ src/waiting.ts
@@ -2,4 +2,13 @@ import * as Phaser from 'phaser';
 import { BaseScene } from './baseScene';
 
 export class Wating extends BaseScene {
+  preload() {
+    this.loadImages({
+      bg: "bg.jpg",
+      soundOn: "soundOn.png", soundOff: "soundOff.png"
+    });
+  }
+  create() {
+    this.add.image(this.canX(0.5), this.canY(0.5), 'bg');
+  }
 }
