54470f2a0c90d252b18a55a5277f208b43de5817
Nabetani <takenori@nabetani.sakura.ne.jp>
2023-12-07 23:36:56 +0900

    ジャンプ

--- src/gameMain.ts
+++ src/gameMain.ts
@@ -2,7 +2,19 @@ import * as Phaser from 'phaser';
 import { BaseScene, Audio } from './baseScene';
 import { Model } from './model';
 
-type Phase = StartPhase;
+/*
+ 縦スクロール
+ カエルジャンプ
+ ジャンプする距離は、ボタンによる
+ ジャンプする方向は、ボタンの向き
+ ボタンはずっと向きを変えつづける。
+ ボタンをクリックするとジャンプと同時にボタンが消滅。消滅分はすぐに供給される。
+ カエルが着地した 蓮の葉（？）は、時間切れで水没。
+ 距離がスコア。
+ */
+
+
+type Phase = StartPhase | GamePhase;
 
 class StartPhase {
   scene: GameMain;
@@ -10,6 +22,18 @@ class StartPhase {
     this.scene = scene;
   }
   progress(): Phase {
+    return new GamePhase(this.scene);
+  }
+}
+
+class GamePhase {
+  scene: GameMain;
+  constructor(scene: GameMain) {
+    this.scene = scene;
+  }
+  progress(): Phase {
+    this.scene.updateArrows();
+    this.scene.updatePlayer();
     return this
   }
 }
@@ -22,11 +46,37 @@ export class GameMain extends BaseScene {
   preload() {
     this.loadImages({
       bg: "bg.webp",
+      arrow: "arrow.webp",
+      p0: "p0.webp",
+      p1: "p1.webp",
     });
   }
   create() {
     console.log("create GameMain")
     this.add.image(...this.canXY(0.5), 'bg');
+    this.addSprite(-100, -100, "p0", "p0");
+    for (let i = 0; i < 8; i++) {
+      const x = ((i % 4) * 2 + 1) * 512 / 8;
+      const y = 800 - Math.floor(i / 4) * 120;
+      const name = `arrow${i}`
+      this.addSprite(x, y, "arrow", name)
+      this.sprites[name].on('pointerdown', () => { this.model.arrowClick(i); }).setInteractive();
+    }
+  }
+  updatePlayer() {
+    this.model.updatePlayer();
+    const p = this.model.player;
+    this.sprites.p0.setPosition(p.pos.x, p.pos.y);
+    this.sprites.p0.setAngle(p.angle);
+    this.sprites.p0.setScale(Math.max(0, p.z / 5) + 1);
+  }
+
+  updateArrows() {
+    this.model.updateArrows();
+    for (let i = 0; i < this.model.arrowsX.length; i++) {
+      this.sprites[`arrow${i}`].setAngle(this.model.arrowAngle(i));
+    }
+
   }
   update() {
     this.phase = this.phase.progress();
--- src/model.ts
+++ src/model.ts
@@ -1,4 +1,52 @@
+import * as Phaser from 'phaser';
+
+type Vector2 = Phaser.Math.Vector2;
+const Vector2 = Phaser.Math.Vector2;
+const v2 = (x: number, y: number): Vector2 => {
+  return new Vector2(x, y);
+}
+
+class Player {
+  state: string = "";
+  pos: Vector2 = v2(200, 500);
+  vel: number = 3;
+  z: number = 0;
+  zVel: number = 0;
+  angle: number = 0;
+};
+
 export class Model {
+  arrowsX: number[] = [0, 0, 0, 0, 0, 0, 0, 0];
+  arrowsV: number[] = [9e-2, 10e-2, 11e-2, 13e-2, 17e-2, 5.3e-2, 7e-2, 6.1e-2,];
+  arrowAngle(i: integer): number {
+    return Math.sin(this.arrowsX[i]) * 90;
+  }
+  player: Player = new Player();
+
   pointerup() { }
   pointerdown() { }
+  arrowClick(i: integer) {
+    if (this.player.z <= 0) {
+      this.player.angle = this.arrowAngle(i);
+      this.player.z = 0.01;
+      this.player.zVel = 1
+    }
+    console.log(this.arrowAngle(i));
+  }
+  updatePlayer() {
+    console.log("z=", this.player.z);
+    if (this.player.z <= 0) {
+      return;
+    }
+    this.player.z += this.player.zVel;
+    this.player.zVel -= 0.03
+    const t = (this.player.angle - 90) * (Math.PI / 180);
+    this.player.pos.x += Math.cos(t) * this.player.vel;
+    this.player.pos.y += Math.sin(t) * this.player.vel;
+  }
+  updateArrows() {
+    for (let i = 0; i < this.arrowsV.length; i++) {
+      this.arrowsX[i] += this.arrowsV[i] * 1e-1;
+    }
+  }
 }
